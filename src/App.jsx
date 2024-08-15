2d8ea8ead1cd33361284183e9ece8cd8e6d5a05e
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-08-15 15:40:32 +0530

    remove sample charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import BookingsChart from "./BookingsChart";
-import FareChart from "./FareChart";
-import CancellationChart from "./CancellationChart";
-import ClassDistributionChart from "./ClassDistributionChart";
-import ClassDistributionChart2 from "./ClassDistributionChart2";
-import ClassDistributionChart3 from "./ClassDistributionChart3";
+
 function App() {
   return (
     <>
-    <h1>Bookings Chart</h1>
-    <BookingsChart />
-    <h1>Fare Chart</h1>
-    <FareChart />
-    <h1>Cancellation Chart</h1>
-    <CancellationChart />
-    <h1>Class Distribution Chart 1</h1>
-    <ClassDistributionChart />
-    <h1>Class Distribution Chart 2</h1>
-    <ClassDistributionChart2 />
-    <h1>Class Distribution Chart 3</h1>
-    <ClassDistributionChart3 />
+    <h1>Admin Panel</h1>
     </>
   );
 }
