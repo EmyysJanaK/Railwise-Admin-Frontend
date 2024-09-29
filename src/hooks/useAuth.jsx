4b8b4936a37971a72d19507bd1b4c109847e1895
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-09-29 22:12:18 +0530

    changed api route for login

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -11,7 +11,7 @@ const useAuth = () => {
 
   const loginUser = async ({ username, password }) => {
     try {
-      const response = await axios.post("http://localhost:3000/api/admin/login", {
+      const response = await axios.post("/api/admin/login", {
         username,
         password,
       }, { withCredentials: true });
