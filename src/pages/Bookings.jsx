d9686fd1fb777327afa00d90430feb225e9e5059
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-10-09 17:38:00 +0530

    Update Bookings.jsx

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -2,21 +2,21 @@ import { Grid, Box, Typography } from "@mui/material";
 import BookingRatesLineChart from "../components/graphs/BookingRatesLineChart";
 
 const Cancellations = () => {
-	return (
-		<Box sx={{ padding: 2 }}>
-			<Typography variant="h4" gutterBottom>
-				Bookings
-			</Typography>
-			<Grid container spacing={2}>
-				<Grid item xs={12}>
-					<BookingRatesLineChart status="cancelled" />
-				</Grid>
-				<Grid item xs={12}>
-					<BookingRatesLineChart status="approved" />
-				</Grid>
-			</Grid>
-		</Box>
-	);
+  return (
+    <Box sx={{ padding: 2 }}>
+      <Typography variant="h4" gutterBottom>
+        Bookings
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <BookingRatesLineChart status="approved" />
+        </Grid>
+        <Grid item xs={12}>
+          <BookingRatesLineChart status="cancelled" />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Cancellations;
